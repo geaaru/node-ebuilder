2ae0bb7243f56a71f4861fb663159fee7717353d
Geaaru <geaaru@gmail.com>
2017-06-13 18:24:45 +0200

    [ebuild] Fix management of versions not compliant with gentoo ebuild - issue #1

--- test/test-vers.js
+++ test/test-vers.js
@@ -4,7 +4,8 @@
 // -----------------------------------------------
 
 var semver = require('semver'),
-   sprintf = require('util').format;
+   sprintf = require('util').format,
+   ebuild = require('../lib/ebuild');
 var path = require('path');
 
 var test_vers1 = function () {
@@ -85,14 +86,38 @@ var test_vers5 = function () {
    console.log(sprintf("patch of version %s: %s", v3, semver.patch(v3)));
 };
 
+var test_range_versions = function() {
+
+   // Handle double-ended-queue version...
+
+   var v = '2.1.0';
+   var p_v = '^2.0.0-beta3';
+
+   console.log(sprintf("ebuild version %s", v));
+
+   console.log(sprintf("packet version %s", p_v));
+
+   console.log(sprintf("Satisfy range: %s", semver.satisfies(v, p_v)));
+
+};
+
+var test_gentoofy1 = function() {
+
+   version = '2.1.0-0';
+
+   console.log(sprintf('gentoo %s => %s', version, ebuild.gentoofyVersion(version)));
+
+}
 
 console.log("BEGIN " + path.basename(__filename, '.js'));
 
-test_vers1();
-test_vers2();
-test_vers3();
-test_vers4();
-test_vers5();
+//test_vers1();
+//test_vers2();
+//test_vers3();
+//test_vers4();
+//test_vers5();
+//test_range_versions();
+test_gentoofy1();
 
 console.log("END " + path.basename(__filename, '.js'));
 
